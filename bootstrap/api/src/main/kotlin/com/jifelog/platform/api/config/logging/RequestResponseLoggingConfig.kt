9e79e191ee596d7285a8a23fa0f83c98385efc5f
haejun-je <jhjun07@gmail.com>
2026-08-23 22:58:57 +0900

    fix: /actuator/** 요청 로깅 제외

--- bootstrap/api/src/main/kotlin/com/jifelog/platform/api/config/logging/RequestResponseLoggingConfig.kt
+++ bootstrap/api/src/main/kotlin/com/jifelog/platform/api/config/logging/RequestResponseLoggingConfig.kt
@@ -1,18 +1,28 @@
 package com.jifelog.platform.api.config.logging
 
 import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.filter.CommonsRequestLoggingFilter
 
 @Configuration
 class RequestResponseLoggingConfig {
+    private val log = LoggerFactory.getLogger(this::class.java)
 
     @Bean
     fun requestLoggingFilter(): CommonsRequestLoggingFilter =
         object : CommonsRequestLoggingFilter() {
             override fun shouldLog(request: HttpServletRequest): Boolean =
                 !request.requestURI.startsWith("/actuator/")
+
+            override fun beforeRequest(request: HttpServletRequest, message: String) {
+                log.info(message)
+            }
+
+            override fun afterRequest(request: HttpServletRequest, message: String) {
+                log.info(message)
+            }
 
             init {
                 isIncludePayload = true
